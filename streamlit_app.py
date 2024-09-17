c39b3ccf0c7e90fc8afad2214aaf4f7adf4437e9
dvasylevskyi <dvasylevskyi@beechhallschool.org>
2024-09-17 17:58:52 +0100

    added your age

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ st.title("🎈 Dmytro")
 st.write("The best site ever")
 name = st.text_input("Please enter your name")
 birthday = st.text_input("Please enter your birthday date")
+your age = st.text_output(17)
 st.write("Welcome ", name)
-st.write(name, "Let's do some Maths")
+st.write
