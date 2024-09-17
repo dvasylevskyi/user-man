db16756869f7ca372ec178f86766d50c098f36e1
dvasylevskyi <dvasylevskyi@beechhallschool.org>
2024-09-17 14:49:15 +0100

    add birthday date

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("🎈 Dmytro")
-st.write("My first python app")
+st.write("The best site ever")
 name = st.text_input("Please enter your name")
+birthday = st.text_input("Please enter your birthday date")
 st.write("Welcome ", name)
